c4f7a0a74b65aeb40ed8667daa8eee08a3b1d8e0
Sai prasad <sp@MacBook-Pro.local>
2021-05-15 15:39:03 +0530

    Add react router and check how it works

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
 import { PostsList, Navbar } from './';
+
+const Login = () => <div>Login</div>;
+const Signup = () => <div>Signup</div>;
+const Home = () => <div>Home</div>;
 
 class App extends React.Component {
   componentDidMount() {
@@ -14,10 +19,28 @@ class App extends React.Component {
     const { posts } = this.props;
 
     return (
-      <div>
-        <Navbar />
-        <PostsList posts={posts} />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          {/* <PostsList posts={posts} /> */}
+          <ul>
+            {/* same as anchor tag..but doesnt refresh the page while redirecting */}
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">signup</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />{' '}
+          {/* or simply exact={true} */}
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </div>
+      </Router>
     );
   }
 }
